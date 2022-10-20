b2ba7e59c915596ba298f51e6e892e422f829b5f
Mark Bailey <markbailey78@yahoo.co.uk>
2022-10-20 22:58:22 +0100

    Added firebase to functions

---
+++ functions/src/firebase.ts
@@ -0,0 +1,31 @@
+require('dotenv').config();
+// import functions from 'firebase-functions';
+import admin from 'firebase-admin';
+
+const DATABASE_URL =
+  'https://go-for-guides-default-rtdb.europe-west1.firebasedatabase.app';
+
+// Initialize the app with a service account, granting admin privileges
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL
+  }),
+  databaseURL: DATABASE_URL
+});
+
+export const ADMIN_UID = process.env.ADMIN_UID;
+export const auth = admin.auth();
+export const db = admin.database();
+
+export async function getSnapshotValue<T>(path: string) {
+  const ref = db.ref(path);
+  let value: T | undefined;
+
+  await ref.once('value', async (snapshot) => {
+    value = snapshot.val();
+  });
+
+  return value;
+}
