96dac8f6964dda58243439a03a58cf7395d6bed2
Mark Bailey <markbailey78@yahoo.co.uk>
2022-10-23 11:46:39 +0100

    Update useDocumentTitle.ts

--- src/hooks/useDocumentTitle.ts
+++ src/hooks/useDocumentTitle.ts
@@ -3,6 +3,7 @@ import appConfig from '../data/app-config.json';
 
 const useDocumentTitle = (title: string) => {
   const [documentTitle, setDocumentTitle] = useState(title);
+
   useEffect(() => {
     const newTitle = `${documentTitle} | ${appConfig.company}`;
     document.title = newTitle;
