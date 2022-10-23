e18575d318632b4421818d678a2e14b878a23fac
Mark Bailey <markbailey78@yahoo.co.uk>
2022-10-23 11:41:16 +0100

    Installed vite-plugin-svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-// import dotenv from 'dotenv';
-// dotenv.config();
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [svgr(), react()]
 });
