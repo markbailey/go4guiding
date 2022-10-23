edc5fdfb91efcba973bdeb3c0a6cfd6bbc8de365
Mark Bailey <markbailey78@yahoo.co.uk>
2022-10-23 11:50:01 +0100

    Update store with db api reducer

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,17 +1,19 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 
 import api from '../services/api';
+import { db } from '../services/firebase';
 import { reducer as authReducer } from './auth';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    [db.reducerPath]: db.reducer,
     [api.reducerPath]: api.reducer
   },
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(api.middleware)
+    getDefaultMiddleware().concat(api.middleware, db.middleware)
 });
 
 export type AppDispatch = typeof store.dispatch;
