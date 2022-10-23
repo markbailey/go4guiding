0341ae1e9c45b80153ea8f4bd2f2ee536e22d153
Mark Bailey <markbailey78@yahoo.co.uk>
2022-10-23 11:46:10 +0100

    Update common.ts

--- src/common.ts
+++ src/common.ts
@@ -1,12 +1,31 @@
+export const ASYNC_TIMEOUT = 6000;
+export const ONE_DAY = 1000 * 60 * 60 * 24;
+
 export enum Role {
   Admin = 'admin',
   Leader = 'leader',
   Member = 'member'
 }
 
-export enum StorageKey {
-  SignInAsEmail = 'signInAs.email',
-  SignInAsOrigin = 'signInAs.origin'
+export enum ThemeName {
+  KnowMyself = 'Know Myself',
+  ExpressMyself = 'Express Myself',
+  BeWell = 'Be Well',
+  HaveAdventures = 'Have Adventures',
+  takeAction = 'Take Action',
+  SkillsForMyFuture = 'Skills For My Future'
 }
 
+export enum ThemeProgram {
+  InterestBadges = 'Interest Badges',
+  SkillsBuilders = 'Skills Builders',
+  UnitMeetingActivities = 'Unit Meeting Activities'
+}
+
+// export enum StorageKey {
+//   SignInAsEmail = 'signInAs.email',
+//   SignInAsOrigin = 'signInAs.origin'
+// }
+
 export const AdminRoles = [Role.Admin, Role.Leader];
+export const MemberRoles = [Role.Leader, Role.Member];
