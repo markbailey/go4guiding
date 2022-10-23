41b6a6febeb6edfca9af3c5be72cb9ae74cb5f48
Mark Bailey <markbailey78@yahoo.co.uk>
2022-10-23 11:50:10 +0100

    Create db.d.ts

---
+++ src/types/db.d.ts
@@ -0,0 +1,45 @@
+// Theme Awards
+declare type ThemeAwardProgram =
+  | 'Interest Badges'
+  | 'Skills Builders'
+  | 'Unit Meeting Activities';
+
+interface Activity {
+  name: string;
+  duration: number;
+}
+
+interface InterestBadge {
+  name: string;
+  activities: string[];
+}
+
+interface SkillsBuilder {
+  name: string;
+  stage: number;
+  activities: Activity[];
+}
+
+interface ThemeData {
+  name: string;
+  description: string;
+  interestBadges: InterestBadge[];
+  skillsBuilders: SkillsBuilder[];
+  unitMeetingActivities: Activity[];
+}
+
+// Awards Targets
+declare interface AwardTargets {
+  interestBadges: number[];
+  skillsBuilders: number[];
+  unitMeetingActivities: number;
+}
+
+// Units
+declare type GuidingProgramme = 'brownies' | 'rainbows' | 'guides' | 'rangers';
+declare interface Unit {
+  uid: string;
+  guidingProgramme: GuidingProgramme;
+  displayName: string;
+  patrols: record<string, string>;
+}
