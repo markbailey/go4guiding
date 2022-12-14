fac2aae544f08dda526bbb13627ea0ae20a2651b
Mark Bailey <markbailey78@yahoo.co.uk>
2022-12-14 14:29:42 +0000

    Update Firebase configs

--- functions/src/firebase.ts
+++ functions/src/firebase.ts
@@ -8,9 +8,9 @@ const DATABASE_URL =
 // Initialize the app with a service account, granting admin privileges
 admin.initializeApp({
   credential: admin.credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL
+    projectId: process.env.PROJECT_ID,
+    privateKey: process.env.PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    clientEmail: process.env.CLIENT_EMAIL
   }),
   databaseURL: DATABASE_URL
 });
