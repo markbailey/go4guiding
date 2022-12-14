0257592586aabd3cbde9dbb0812d67976b7295eb
Mark Bailey <markbailey78@yahoo.co.uk>
2022-12-14 23:15:38 +0000

    Update api.ts

--- src/services/api/api.ts
+++ src/services/api/api.ts
@@ -15,7 +15,7 @@ const api = createApi({
     prepareHeaders: (headers, { getState }) => {
       const { token } = (getState() as RootState).auth;
       // If we have a token set in state, let's assume that we should be passing it.
-      if (token) headers.set('Authorization', `Bearer ${token}`);
+      if (token !== null) headers.set('Authorization', `Bearer ${token}`);
       headers.set('Content-Type', 'application/json');
       return headers;
     }
