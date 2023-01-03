71f2e3c6370cc69d99c49f3f4e5747ec054e1a1d
Mark Bailey <markbailey78@yahoo.co.uk>
2023-01-03 20:50:24 +0000

    Updated hooks exports

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,1 +1,6 @@
 export * from './hooks';
+export { default as useDocumentTitle } from './useDocumentTitle';
+export { default as useEventListener } from './useEventListener';
+export { default as useMediaQuery } from './useMediaQuery';
+export { default as useOnlineStatus } from './useOnlineStatus';
+export { useSessionStorage, useLocalStorage } from './useStorage';
